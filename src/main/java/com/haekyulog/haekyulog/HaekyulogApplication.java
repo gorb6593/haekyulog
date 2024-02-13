697f95b5dc81b890c7f4eedf7af9013b3717c225
haekyu <gorb6593@gmail.com>
2024-02-13 22:38:46 +0900

    Initial commit

---
+++ src/main/java/com/haekyulog/haekyulog/HaekyulogApplication.java
@@ -0,0 +1,13 @@
+package com.haekyulog.haekyulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaekyulogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaekyulogApplication.class, args);
+    }
+
+}
