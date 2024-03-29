88d77de2d57d06db7380e05f4ffc09c1ac7fa055
haekyu <gorb6593@gmail.com>
2024-03-29 20:06:16 +0900

    쿠키를 사용한 토큰 발급

--- src/main/java/com/haekyulog/haekyulog/config/AuthResolver.java
+++ src/main/java/com/haekyulog/haekyulog/config/AuthResolver.java
@@ -4,13 +4,16 @@ import com.haekyulog.haekyulog.config.data.UserSession;
 import com.haekyulog.haekyulog.domain.Session;
 import com.haekyulog.haekyulog.exception.Unauthorized;
 import com.haekyulog.haekyulog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -21,13 +24,38 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
         return parameter.getParameterType().equals(UserSession.class);
     }
 
+    //토큰방식
+//    @Override
+//    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+//        String accessToken = webRequest.getHeader("Authorization");
+//
+//        if (accessToken == null || accessToken.equals("")) {
+//            throw new Unauthorized();
+//        }
+//
+//        Session session = sessionRepository.findByAccessToken(accessToken)
+//                .orElseThrow(Unauthorized::new);
+//
+//        return new UserSession(session.getUsers().getId());
+//    }
+    //쿠키방식
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String accessToken = webRequest.getHeader("Authorization");
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
 
-        if (accessToken == null || accessToken.equals("")) {
+        if (servletRequest == null) {
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if (cookies.length == 0) {
+            log.error("no cookie");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
--- src/main/java/com/haekyulog/haekyulog/controller/AuthController.java
+++ src/main/java/com/haekyulog/haekyulog/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.haekyulog.haekyulog.controller;
 
 import com.haekyulog.haekyulog.requesst.Login;
-import com.haekyulog.haekyulog.response.SessionResponse;
 import com.haekyulog.haekyulog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -17,16 +21,36 @@ public class AuthController {
     //private final UserRepository userRepository;
     private final AuthService authService;
 
-    @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
-        //json 아이디/비밀번호
-         log.info(">>> login : {}" , login);
-        //db에서 조회
-//        Users users = userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
-//                .orElseThrow(InvalidSigninInformation::new);
-        String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
-        //토큰을 응답
+    //토큰 버전
+//    @PostMapping("/auth/login")
+//    public SessionResponse login(@RequestBody Login login) {
+//        //json 아이디/비밀번호
+//         log.info(">>> login : {}" , login);
+//        //db에서 조회
+////        Users users = userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
+////                .orElseThrow(InvalidSigninInformation::new);
+//        String accessToken = authService.signin(login);
+//        return new SessionResponse(accessToken);
+//        //토큰을 응답
+//
+//    }
 
+    //쿠키버전
+    @PostMapping("/auth/login")
+    public ResponseEntity<Object> login(@RequestBody Login login) {
+        String accessToken = authService.signin(login);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") //todo 서버 환경에 따른 분리 필요
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>>> cookie = {} ", cookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
