484a6a3094fd5be17d95b4979be523ad629ea75b
haekyu <gorb6593@gmail.com>
2024-04-10 19:58:58 +0900

    스프링 시큐리티 설정

--- src/main/java/com/haekyulog/haekyulog/controller/AuthController.java
+++ src/main/java/com/haekyulog/haekyulog/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.haekyulog.haekyulog.requesst.Signup;
 import com.haekyulog.haekyulog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,5 +79,10 @@ public class AuthController {
     @PostMapping("/auth/signup")
     public void signup(@RequestBody Signup signup) {
         authService.signup(signup);
+    }
+
+    @GetMapping("/auth/login")
+    public String login () {
+        return "로그인페이지입니다.";
     }
 }
