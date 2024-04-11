42133fe25daa8090632093f4c57ed634e32d0bb0
haekyu <gorb6593@gmail.com>
2024-04-11 09:41:11 +0900

    시큐리티 설정 예외처리

--- src/main/java/com/haekyulog/haekyulog/config/SecurityConfig.java
+++ src/main/java/com/haekyulog/haekyulog/config/SecurityConfig.java
@@ -4,20 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/favicon.ico")
+                .requestMatchers("error")
+                .requestMatchers(toH2Console());
+//                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        return http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
-                )
+                ).build()
         ;
-        return http.build();
+
     }
 }
